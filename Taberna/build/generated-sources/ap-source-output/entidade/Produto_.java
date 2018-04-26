60e94b4d768744d89a630f0045c47c6758d061e0
Anatoli <anatoliandrei@hotmail.com>
2018-04-26 09:42:16 -0300

    v1.0

---
+++ Taberna/build/generated-sources/ap-source-output/entidade/Produto_.java
@@ -0,0 +1,18 @@
+package entidade;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-04-24T11:48:51")
+@StaticMetamodel(Produto.class)
+public class Produto_ { 
+
+    public static volatile SingularAttribute<Produto, String> marca;
+    public static volatile SingularAttribute<Produto, String> tipo;
+    public static volatile SingularAttribute<Produto, Long> valor;
+    public static volatile SingularAttribute<Produto, String> nome;
+    public static volatile SingularAttribute<Produto, Long> id;
+    public static volatile SingularAttribute<Produto, String> descricao;
+
+}
