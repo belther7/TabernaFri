e2ff3dbceee7a9782f9d095fdb1424b603a6d281
Anatoli <anatoliandrei@hotmail.com>
2018-05-01 19:47:42 -0300

    adição do segundo web services

--- Taberna/build/generated-sources/ap-source-output/entidade/Produto_.java
+++ Taberna/build/generated-sources/ap-source-output/entidade/Produto_.java
@@ -4,7 +4,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-04-24T11:48:51")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-05-01T19:38:00")
 @StaticMetamodel(Produto.class)
 public class Produto_ { 
 
